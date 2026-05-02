a5b8fb31a5879a8654ed498cc1668b5ae516fb13
Emre Şurğun <emresurgun1@gmail.com>
2026-05-03 00:15:28 +0300

    temporary CentralApi for Scheduler development.

--- aws-location-performance-benchmark/benchmark-agent/src/main/java/io/github/emresurgun/benchmark/agent/client/CentralApiClient.java
+++ aws-location-performance-benchmark/benchmark-agent/src/main/java/io/github/emresurgun/benchmark/agent/client/CentralApiClient.java
@@ -1,4 +1,23 @@
 package io.github.emresurgun.benchmark.agent.client;
 
+import io.github.emresurgun.benchmark.agent.model.MeasurementResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CentralApiClient {
+    private static final Logger log = LoggerFactory.getLogger(CentralApiClient.class);
+
+    public void send(MeasurementResult result)
+    {
+        log.info(
+                "Measurement result: target={}, metric={}, valueMs={}, success={}",
+                result.getTargetRegion(),
+                result.getMetricType(),
+                result.getValueMs(),
+                result.isSuccess()
+        );
+    }
+
 }
